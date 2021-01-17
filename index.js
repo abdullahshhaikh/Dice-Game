2bf572de156161c2cd0925b8394709a008772be3
AbdullahShaikh <77590186+abdullahshhaikh@users.noreply.github.com>
2021-01-18 03:11:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var randomNumber0 = Math.floor(Math.random() * 6 + 1);
+document.querySelectorAll("img")[0].setAttribute("src","images/dice" + randomNumber0 + ".png");
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+document.querySelectorAll("img")[1].setAttribute("src","images/dice" + randomNumber1 + ".png");
+
+if (randomNumber0 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if(randomNumber0 < randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
